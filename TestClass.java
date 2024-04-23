5ddbb0786d02d5be7a6af8c8edc40ae25f624037
Victoria Gardella <108140180+VGardella@users.noreply.github.com>
2024-04-23 15:37:07 -0300

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass  
+{  
+// main method  
+public static void main(String []args)  
+{  
+// print statement  
+System.out.println("Hello World is my first Java Program.");  
+}  
+} 
